e930e5cc726d2333d69ab9971adc0614fd04b556
yosiamario <gaker3928@gmail.com>
2026-05-13 15:01:36 +0700

    feat: implement auction listing page and payment flow architecture

--- app.js
+++ app.js
@@ -1,4 +1,6 @@
 const express = require('express');
+const cors = require('cors');
+const path = require('path');
 const app = express();
 const { sql, getConnection } = require('./config/db');
 
@@ -6,13 +8,19 @@ const { sql, getConnection } = require('./config/db');
 const authRoutes = require('./routes/authRoutes');
 const barangRoutes = require('./routes/barangRoutes');
 const adminRoutes = require('./routes/adminRoutes')
+const pembayaranRoutes = require('./routes/pembayaranRoutes');
 
+app.use(cors());
 app.use(express.json());
+
+// Serve static files (HTML, CSS, JS, images)
+app.use(express.static(path.join(__dirname)));
 
 // Gunakan Routes
 app.use('/auth', authRoutes);
 app.use('/barang', barangRoutes);
 app.use('/admin', adminRoutes);
+app.use('/pembayaran', pembayaranRoutes);
 
 const PORT = 3000;
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+app.listen(PORT, () => console.log(`Server running on http://localhost:${PORT}`));
