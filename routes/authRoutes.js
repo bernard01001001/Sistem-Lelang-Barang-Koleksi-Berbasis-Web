b78e580239bc778b5fd8663362cba00f2e84256d
yosiamario <gaker3928@gmail.com>
2026-05-16 02:36:49 +0700

    Update authRoutes.js

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -25,6 +25,9 @@ router.post('/signup', async (req, res) => {
             user: result.rows[0] 
         });
     } catch (err) {
+        if (err.code === '23505') {
+            return res.status(400).json({ error: "Email telah digunakan" });
+        }
         res.status(500).json({ error: err.message });
     }
 });
