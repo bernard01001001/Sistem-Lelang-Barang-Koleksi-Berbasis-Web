a702804c0b634d9c1e090cf1fbd680fea893308f
yosiamario <gaker3928@gmail.com>
2026-05-16 00:14:54 +0700

    feat: add product listing page and implement server-side item creation routes

--- app.js
+++ app.js
@@ -2,14 +2,11 @@ require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
 const path = require('path');
-app.use(express.static(path.join(__dirname, '.')));
-app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, 'index.html'));
-});
-module.exports = app;
+
 const app = express();
 
 app.use(express.json());
+app.use(cors());
 
 // Import Routes
 const authRoutes = require('./routes/authRoutes');
@@ -18,15 +15,18 @@ const adminRoutes = require('./routes/adminRoutes');
 const lelangRoutes = require('./routes/lelangRoutes');
 const pembayaranRoutes = require('./routes/pembayaranRoutes');
 
-
 // Serve static files (HTML, CSS, JS, images)
 app.use(express.static(path.join(__dirname)));
+
+// Route utama untuk meload index.html
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'index.html'));
+});
 
 // Gunakan Routes
 app.use('/auth', authRoutes);
 app.use('/barang', barangRoutes);
 app.use('/admin', adminRoutes);
-
 app.use('/lelang', lelangRoutes);
 app.use('/pembayaran', pembayaranRoutes);
 
